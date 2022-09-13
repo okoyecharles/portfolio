af5750eba2ef9a59fb29e8ec719bd71b23cdca0b
OkoyeCharles <okoyecharles509@gmail.com>
2022-09-13 19:53:27 +0100

    Modify navigation component for mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes } from "react-router-dom";
 import Header from "./components/Header";
 
 const App: React.FC = () => {
+  const [mediaWidth, setMediaWidth] = useState<number>(0);
+  useEffect(() => {
+    setMediaWidth(window.innerWidth);
+  }, []);
+  window.addEventListener("resize", (e: Event) => {
+    const window = e?.target as Window;
+    setMediaWidth(window.innerWidth);
+  });
   return (
     <div className="App">
-      <Header />
-      <Routes>
-      </Routes>
+      <Header mediaWidth={mediaWidth} />
+      <Routes></Routes>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { IoIosMenu, IoIosClose } from "react-icons/io";
 import "../styles/Header.css";
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  mediaWidth: number;
+}
+
+const Header: React.FC<HeaderProps> = ({ mediaWidth }) => {
   const navigate = useNavigate();
+  const [mobileNavActive, setMobileVavActive] = useState<boolean>(false);
   return (
     <header className="header">
-      <div className="header__logo" onClick={() => navigate('/')}>OKOYE CHARLES</div>
+      <div className="header__logo" onClick={() => navigate("/")}>
+        OKOYE CHARLES
+      </div>
 
-      <nav className="header__nav" aria-roledescription="navigation">
+      {mediaWidth <= 700 && (
+        <IoIosMenu
+          className="header__navToggle"
+          onClick={() => {
+            setMobileVavActive(true);
+          }}
+        />
+      )}
+
+      <nav className={mobileNavActive ? "header__nav active" : "header__nav"} aria-roledescription="navigation">
+        {mediaWidth <= 700 && (
+          <IoIosClose
+            className="header__navToggle header__navToggle--close"
+            onClick={() => {
+              setMobileVavActive(false);
+            }}
+          />
+        )}
         <ul className="header__navLinks">
           <li>
             <a href="#">Home</a>
